cec783d87160baf20b85739753b821827f0e4a7e
SpaceXDragon13 <sevensteve13@gmail.com>
2024-02-22 00:00:13 +0800

    Changes to be committed: 	modified:   lib/screens/CameraPage.dart 	modified:   lib/screens/LearnMore.dart 	modified:   macos/Flutter/GeneratedPluginRegistrant.swift 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/screens/CameraPage.dart
+++ lib/screens/CameraPage.dart
@@ -3,11 +3,13 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+import 'package:tomatoapp/screens/LearnMore.dart';
 
 late List<CameraDescription> cameras;
 
 class CameraPage extends StatefulWidget {
-  const CameraPage({super.key});
+  const CameraPage({Key? key});
 
   @override
   State<CameraPage> createState() => _CameraPageState();
@@ -16,6 +18,8 @@ class CameraPage extends StatefulWidget {
 class _CameraPageState extends State<CameraPage> {
   late CameraController _controller;
   double _zoomLevel = 0.0;
+  bool _sliderVisible = true;
+  bool _learnMoreEnabled = false;
 
   // For picker
   File? galleryFile;
@@ -61,43 +65,51 @@ class _CameraPageState extends State<CameraPage> {
       body: Center(
         child: Stack(
           children: [
-            Positioned(
-              width: MediaQuery.of(context).size.width * 0.95,
-              top: MediaQuery.of(context).size.height / 1.68,
-              child: Slider(
-                value: _zoomLevel,
-                min: 0.0,
-                max: 3.0,
-                onChanged: (value) {
-                  setState(() {
-                    _zoomLevel = value;
-                    _controller.setZoomLevel(_zoomLevel);
-                  });
-                },
-              ),
-            ),
-            Stack(
-              alignment: Alignment.center, // Align the children to the center
-              children: [
-                Positioned(
-                  top: MediaQuery.of(context).size.height / 10,
-                  left: MediaQuery.of(context).size.width / 2 -
-                      MediaQuery.of(context).size.width * 0.45,
-                  child: Container(
-                    width: MediaQuery.of(context).size.width *
-                        0.9, // 80% of the screen width
-                    height: MediaQuery.of(context).size.height *
-                        0.5, // 50% of the screen height
-                    decoration: BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        color: const Color(0xFFD9D9D9),
-                        borderRadius: BorderRadius.circular(10)),
-                    child: galleryFile == null
-                        ? CameraPreview(_controller..setZoomLevel(_zoomLevel))
-                        : Center(child: Image.file(galleryFile!)),
-                  ),
+            if (_sliderVisible)
+              Positioned(
+                width: MediaQuery.of(context).size.width * 0.95,
+                top: MediaQuery.of(context).size.height / 1.68,
+                child: Slider(
+                  value: _zoomLevel,
+                  min: 0.0,
+                  max: 3.0,
+                  onChanged: (value) {
+                    setState(() {
+                      _zoomLevel = value;
+                      _controller.setZoomLevel(_zoomLevel);
+                    });
+                  },
                 ),
-              ],
+              ),
+            GestureDetector(
+              onTap: _captureImage,
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Positioned(
+                    top: MediaQuery.of(context).size.height / 10,
+                    left: MediaQuery.of(context).size.width / 2 -
+                        MediaQuery.of(context).size.width * 0.45,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width *
+                          0.9, // 80% of the screen width
+                      height: MediaQuery.of(context).size.height *
+                          0.5, // 50% of the screen height
+                      decoration: BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          color: const Color(0xFFD9D9D9),
+                          borderRadius: BorderRadius.circular(10)),
+                      child: galleryFile == null
+                          ? CameraPreview(_controller
+                            ..setZoomLevel(_zoomLevel)
+                            ..startImageStream((image) {
+                              // Do something with the image stream
+                            }))
+                          : Image.file(galleryFile!),
+                    ),
+                  ),
+                ],
+              ),
             ),
             Align(
               alignment: Alignment.centerLeft,
@@ -132,7 +144,13 @@ class _CameraPageState extends State<CameraPage> {
               child: Padding(
                 padding: const EdgeInsets.only(left: 16, bottom: 16),
                 child: ElevatedButton(
-                  onPressed: null,
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                LearnMore(image: galleryFile)));
+                  },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: const Color.fromARGB(255, 29, 168, 47),
                     foregroundColor: Colors.white,
@@ -182,10 +200,26 @@ class _CameraPageState extends State<CameraPage> {
         if (xfilePick != null) {
           galleryFile = File(pickedFile!.path);
         } else {
-          ScaffoldMessenger.of(context).showSnackBar(// is this context <<<
+          ScaffoldMessenger.of(context).showSnackBar(
               const SnackBar(content: Text('Nothing is selected')));
         }
       },
     );
+  }
+
+  Future<void> _captureImage() async {
+    if (!_controller.value.isInitialized) {
+      return;
+    }
+
+    final XFile file = await _controller.takePicture();
+    setState(() {
+      galleryFile = File(file.path);
+      _sliderVisible = false; // Hide the slider after capturing
+      _learnMoreEnabled = true;
+    });
+
+    // Save the image to the photo library
+    await GallerySaver.saveImage(file.path);
   }
 }
--- lib/screens/LearnMore.dart
+++ lib/screens/LearnMore.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:tomatoapp/screens/CameraPage.dart';
-import 'package:tomatoapp/screens/MyHomePage.dart';
 
 class LearnMore extends StatelessWidget {
   final File? image;
@@ -10,7 +8,7 @@ class LearnMore extends StatelessWidget {
 
   Widget _buildDescriptionText() {
     return const Text(
-      'Leaf symptoms of late blight first appear as small, water-soaked areas that rapidly enlarge to form purple-brown, oily-appearing blotches. On the lower side of leaves, rings of grayish white mycelium and spore-forming structures may appear around the blotches.',
+      'Leaf symptoms of late blight first appear as small, water-soaked areas that rapidly enlarge to form purple-brown, oily-appearing blotches. On the lower side of leaves, rings of grayish white mycelium and spore-forming structures may appear around the blotches.Leaf symptoms of late blight first appear as small, water-soaked areas that rapidly enlarge to form purple-brown, oily-appearing blotches. On the lower side of leaves, rings of grayish white mycelium and spore-forming structures may appear around the blotches.Leaf symptoms of late blight first appear as small, water-soaked areas that rapidly enlarge to form purple-brown, oily-appearing blotches. On the lower side of leaves, rings of grayish white mycelium and spore-forming structures may appear around the blotches.',
       style: TextStyle(
         fontSize: 13,
         fontWeight: FontWeight.w400,
@@ -22,128 +20,72 @@ class LearnMore extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFF38384E),
       body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Container(
-              padding: const EdgeInsets.fromLTRB(28, 27, 38, 29),
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: const Color(0xFF38384E),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(0, 0, 0, 25),
-                    width: 32,
-                    height: 32,
-                    child: IconButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      icon: const Icon(Icons.arrow_back, color: Colors.white),
-                    ),
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width - 66, 
-                    height: (MediaQuery.of(context).size.width - 66) * 250 / 290,
-                    child: Image.asset(
-                      'assets/images/late.jpg',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(12, 0, 78, 6),
-                    width: double.infinity,
-                    child: const Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Predicted Disease: ',
-                          style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.w700,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          'Healthy',
-                          style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.w700,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(12, 0, 98, 15),
-                    width: double.infinity,
-                    child: const Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Confidence Level:',
-                          style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.w700,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          '0.90',
-                          style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.w700,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(9, 0, 0, 0),
-                    constraints: const BoxConstraints(maxWidth: 284),
-                    child: _buildDescriptionText(),
-                  ),
-                ],
+              margin: EdgeInsets.fromLTRB(
+                  0, 25, 0, MediaQuery.of(context).size.height * 0.05),
+              width: 32,
+              height: 32,
+              child: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(Icons.arrow_back, color: Colors.white),
               ),
             ),
             Container(
-              padding: const EdgeInsets.fromLTRB(81, 16, 78, 16),
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                color: Color(0xffd9d9d9),
+              margin: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).size.height * 0.03),
+              width: MediaQuery.of(context).size.width *
+                  0.9, // 80% of the screen width
+              height: MediaQuery.of(context).size.height *
+                  0.4, // 50% of the screen height
+              child: Image.file(image!),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width * 0.05),
+              child: Container(
+                margin: const EdgeInsets.fromLTRB(12, 0, 78, 6),
+                width: double.infinity,
+                child: const Text(
+                  'Predicted Disease: ',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
+                ),
               ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const MyHomePage()),
-                      );
-                    },
-                    icon: const Icon(Icons.home, color: Colors.black),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width * 0.05),
+              child: Container(
+                margin: const EdgeInsets.fromLTRB(12, 0, 98, 6),
+                width: double.infinity,
+                child: const Text(
+                  'Confidence Level: ',
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
                   ),
-                  IconButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const CameraPage()),
-                      );
-                    },
-                    icon: const Icon(Icons.camera_alt_rounded, color: Colors.black),
-                  ),
-                ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width * 0.05),
+              child: Container(
+                margin: EdgeInsets.fromLTRB(
+                    9, 0, 0, MediaQuery.of(context).size.height * 0.05),
+                constraints: const BoxConstraints(maxWidth: 284),
+                child: _buildDescriptionText(),
               ),
             ),
           ],
